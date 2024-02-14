c734bdac2f9f4750e12b12a02846e7775cad449a
kokoichi206 <tt.300607@gmail.com>
2024-02-15 01:45:58 +0900

    workers から d1 に情報を保存する

--- honosu/README.md
+++ honosu/README.md
@@ -10,6 +10,27 @@ wrangler secret put CHANNEL_ACCESS_TOKEN
 npm run deploy
 ```
 
+## D1
+
+``` sh
+wrangler d1 create line-bot
+
+# local db
+wrangler d1 execute line-bot --local --file=./migrations/01_create.sql
+wrangler d1 execute line-bot --local --command="INSERT INTO messages (item) VALUES ('pien');"
+wrangler d1 execute line-bot --local --command='SELECT * FROM messages;'
+
+# cloud!
+wrangler d1 execute line-bot --file=./migrations/01_create.sql
+wrangler d1 execute line-bot --command='SELECT * FROM messages;'
+```
+
 ## Links
 
+- Messaging APIリファレンス
+  - https://developers.line.biz/ja/reference/messaging-api/#wh-image
+- 画像
+  - 別コンテントとして取得が必要そう
+  - https://developers.line.biz/ja/reference/messaging-api/#get-content
 - [CloudFlare Workers、Cloudflare D1、HonoでLINE botを作りました](https://tkancf.com/blog/creating-line-bot-with-cloudflare-workers-d1-and-hono/)
+- [Cloudflare D1](https://developers.cloudflare.com/d1/)
--- honosu/src/index.ts
+++ honosu/src/index.ts
@@ -1,7 +1,12 @@
-import { MessageAPIResponseBase, TextMessage, WebhookEvent } from '@line/bot-sdk';
+import { ImageEventMessage, MessageAPIResponseBase, TextMessage, WebhookEvent } from '@line/bot-sdk';
 import { Hono } from 'hono';
+import { Buffer } from 'buffer';
 
-const app = new Hono();
+type Bindings = {
+	DB: D1Database;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.get('*', (c) => c.text('Hi !!'));
 
@@ -15,7 +20,7 @@ app.post('/api/webhook', async (c) => {
 	await Promise.all(
 		events.map(async (event: WebhookEvent) => {
 			try {
-				await textEventHandler(event, accessToken);
+				await messageEventHandler(event, accessToken, c.env.DB);
 			} catch (err: unknown) {
 				if (err instanceof Error) {
 					console.error(err);
@@ -29,16 +34,35 @@ app.post('/api/webhook', async (c) => {
 	return c.json({ message: 'ok' });
 });
 
-const textEventHandler = async (event: WebhookEvent, accessToken: string): Promise<MessageAPIResponseBase | undefined> => {
-	if (event.type !== 'message' || event.message.type !== 'text') {
+const messageEventHandler = async (
+	event: WebhookEvent,
+	accessToken: string,
+	db: D1Database
+): Promise<MessageAPIResponseBase | undefined> => {
+	if (event.type !== 'message') {
 		return;
 	}
-
 	const { replyToken } = event;
-	const { text } = event.message;
+
+	switch (event.message.type) {
+		case 'text':
+			return textMessageHandler(event.message.text, replyToken, accessToken, db);
+		case 'image':
+			return imageMessageHandler(event.message, replyToken, accessToken, db);
+	}
+};
+
+const textMessageHandler = async (
+	message: string,
+	replyToken: string,
+	accessToken: string,
+	db: D1Database
+): Promise<MessageAPIResponseBase | undefined> => {
+	let rep = message + message;
+
 	const response: TextMessage = {
 		type: 'text',
-		text,
+		text: rep,
 	};
 	await fetch('https://api.line.me/v2/bot/message/reply', {
 		body: JSON.stringify({
@@ -51,6 +75,59 @@ const textEventHandler = async (event: WebhookEvent, accessToken: string): Promi
 			'Content-Type': 'application/json',
 		},
 	});
+
+	// db に入れてみるが、特に意味はない。
+	const dres = await db.prepare('INSERT INTO messages (item) VALUES (?)').bind(message).run();
+	console.log('----- dres -----');
+	console.log(dres);
+
+	return;
+};
+
+const imageMessageHandler = async (
+	message: ImageEventMessage,
+	replyToken: string,
+	accessToken: string,
+	db: D1Database
+): Promise<MessageAPIResponseBase | undefined> => {
+	// > このエンドポイントは、WebhookイベントオブジェクトのcontentProvider.typeプロパティがlineの場合にのみ利用できます。
+	// see: https://developers.line.biz/ja/reference/messaging-api/#getting-content
+	if (message.contentProvider.type !== 'line') {
+		return;
+	}
+
+	const res = await fetch(`https://api-data.line.me/v2/bot/message/${message.id}/content`, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${accessToken}`,
+		},
+	});
+	const buffer = Buffer.from(await res.arrayBuffer());
+	const base64text = buffer.toString('base64');
+
+	// 5000 文字（~3.4 kB の画像?）が上限。
+	const response: TextMessage = {
+		type: 'text',
+		text: base64text.slice(0, 5000),
+	};
+
+	const pien = await fetch('https://api.line.me/v2/bot/message/reply', {
+		body: JSON.stringify({
+			replyToken: replyToken,
+			messages: [response],
+		}),
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${accessToken}`,
+			'Content-Type': 'application/json',
+		},
+	});
+
+	// const dres = await db.prepare('INSERT INTO messages (item) VALUES (?)').bind(base64text).run();
+	// console.log('----- dres -----');
+	// console.log(dres);
+
+	return;
 };
 
 export default app;
